72df52b78080045d01d8c2730e7262f7a735a753
PandaBoo <thuyenle1307@gmail.com>
2025-10-10 19:53:48 +0700

    fix sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage/home";
 import RegisterPage from "./components/pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
