a910caaac68ade195319952eec099ce316147b28
viendinolove <hodang159hd@gmail.com>
2025-12-30 19:56:08 +0700

    phanquyen

--- src/configs/apiConfig.js
+++ src/configs/apiConfig.js
@@ -1,4 +1,3 @@
-// src/configs/apiConfig.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -7,5 +6,30 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// 🔐 Gắn token cho mọi request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// ❗ Bắt lỗi 401 toàn cục (optional)
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Token hết hạn hoặc chưa đăng nhập");
+      // localStorage.removeItem("token");
+      // window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
